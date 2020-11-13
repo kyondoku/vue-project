764ac02a0ccf03efa912ad4a271b8d0544caecfe
kyondoku <kyondoku@gmail.com>
2020-11-13 10:22:57 +0900

    first commit

---
+++ todo-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TodoHeader/>
+    <TodoInput @sendTodo="parentSendTodo"/>
+    {{ todoList }}
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+
+export default {
+  name: 'App',
+  components: { TodoHeader, TodoInput },
+  data() {
+    return {
+      id: 0,
+      todoList: []
+    }
+  },
+  methods: {
+    parentSendTodo(todo) {
+      var obj = {
+        id: ++this.id,
+        todo: todo
+      }
+      this.todoList.push(obj)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ todo-app/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <input type="text" v-model="todo" @keyup.enter="reg">
+        <button @click="reg">등록</button>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            todo: ''
+        }
+    },
+    methods: {
+        reg() {
+            if(this.todo == '') {
+                alert('내용을 입력해 주세요.')
+                return
+            }
+            this.$emit('sendTodo', this.todo)
+            this.todo = ''
+        }
+    }
+}
+</script>
